2ad772e25646fb3caa93a888e40f786a2d4da780
kgerakianaki <mkg@entersoft.gr>
2025-02-06 14:13:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tooltip',
+  webDir: 'www'
+};
+
+export default config;
